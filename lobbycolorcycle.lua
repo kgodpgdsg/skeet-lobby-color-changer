56d1452259854880667241052c30721a66c4390c
kgodpgdsg <90220849+kgodpgdsg@users.noreply.github.com>
2021-11-01 21:04:42 +0100

    Update lobbycolorcycle.lua

--- lobbycolorcycle.lua
+++ lobbycolorcycle.lua
@@ -1,12 +1,11 @@
 local js = panorama.open()
-local lobbyapi = js.LobbyAPI
 local enable = ui.new_checkbox("Misc", "Miscellaneous", "Rainbow lobby")
 local speed = ui.new_slider("Misc", "Miscellaneous", "Switch speed", 1, 100)
 
 client.set_event_callback("paint_ui",  function()
     local newtime = string.format("%.0f",globals.realtime() % 60 * ui.get(speed)/5)
     if ui.get(enable) and newtime ~= oldtime then
-        lobbyapi.ChangeTeammateColor()
+        js.LobbyAPI.ChangeTeammateColor()
         oldtime = newtime
     end
 end)
